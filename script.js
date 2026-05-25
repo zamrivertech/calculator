59d376dc8af83004236397bc7afc2de30fecb99d
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-25 21:32:26 +0200

    Implement and test operate function

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num1, num2, operator,) {
+
+    //use case
+
+    switch (operator) {
+        case "+":
+            return add(num1,num2);
+        break;   
+        case "-":
+            return subtract(num1,num2);
+        break;    
+        case "*":
+            return multiply(num1,num2);
+        break;   
+        case "/":
+            return divide(num1,num2);
+        break;                           
+    }
+
+}
+
+/*
+
+console.log(operate(1,1, "+"));
+console.log(operate(1,1, "-"));
+console.log(operate(1,1, "*"));
+console.log(operate(1,1, "/"));
+
+*/
