f78b62fad2eb0d93bbe0fcdf4da107c7b8014331
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-29 22:28:19 +0200

    Reset DOM mainpulation and button ui

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 let num1 = null;
-let operator = null;
-let tempNumber = null;
 let num2 = null;
-let product = 0;
-
-let screen = document.querySelector(".screen");
-const digitDiv = document.querySelector('#digits');
-const operationDiv = document.querySelector('#operations');
-const digitDisplay = digitDiv.querySelectorAll('button');
-const operateButtons = operationDiv.querySelectorAll('button');
-const result = document.querySelector('#result');
-
-
-//console.log(buttons);
-
+let operator = null;
 
 function add(a, b) {
     return a + b;
@@ -33,8 +20,6 @@ function divide(a, b) {
 
 function operate(num1, num2, operator) {
 
-    //use case
-
     switch (operator) {
         case "+":
             return add(num1,num2);
@@ -53,88 +38,3 @@ function operate(num1, num2, operator) {
     }
 
 }
-
-function displayDigit(buttons) {
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-
-           tempNumber = screen.textContent = Number(screen.textContent + button.id);
-            
-        });
-        
-    });
-
-}
-function setOperator(buttons) {
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-
-        screen.textContent = 0; 
-
-        if (operator != null) {
-
-            console.log('num1 ' + num1 )
-            console.log('num2 ' + num2 )
-            
-            //catch num2, i saw something
-
-            result.click();
-
-        } else {
-
-        num1 = tempNumber; 
-        
-        console.log(num1);        
-        
-        operator = button.id;
-
-        console.log(button.id);
-
-        
-}
-        
-            
-        });
-        
-    });
-
-
-}
-
-function displayResult(result) {
-
-result.addEventListener('click', () => {
-
-    num2 = tempNumber;
-
-    console.log(num2);
-
-    screen.textContent = operate(num1,num2,operator);
-
-});
-
-}
-
-//you have to separate the type of buttons
-//if a digit button is clicked then set number
-//do not query select all buttons in the dom
-
-displayDigit(digitDisplay);
-setOperator(operateButtons);
-displayResult(result);
-
-
-
-
-//setOperator(buttons);
-
-/*
-
-console.log(operate(1,1, "+"));
-console.log(operate(1,1, "-"));
-console.log(operate(1,1, "*"));
-console.log(operate(1,1, "/"));
-
-*/
