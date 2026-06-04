5c96954c9faecca8082ae5f028fb7a01c2fa41e5
zamrivertech <mohamadtasul8000@gmail.com>
2026-06-04 10:10:29 +0200

    Remove c keydown to clear screen

--- script.js
+++ script.js
@@ -247,9 +247,6 @@ function pressedKey() {
 
     calculator.addEventListener('keydown', (e) => {
 
-        if (e.key = "c") {
-            clearButton.click();
-        }
 
         for (let i = 0; i < arrayNum.length; i++) {
             if (e.key === arrayNum[i]) {
